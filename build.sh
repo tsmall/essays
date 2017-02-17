0655393c6624f98f4a99a83897891b5ab9362581
Tom Small <tsmall3@gmail.com>
2017-02-16 23:05:21 -0500

    edit(build): change the site title

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 expose="`pwd`/expose/expose.sh"
-export site_title="Photo Essays"
+export site_title="Smallgren Stories"
 export theme_dir="theme1-custom"
 #export resourcepath_prefix="https://s3.amazonaws.com/smallgren-stories/"
 #export asset_url_prefix="/stories/r/"
