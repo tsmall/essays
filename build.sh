9a217b983414a221fa57fbf41765f8ae42f6ceba
Tom Small <tsmall3@gmail.com>
2017-02-16 23:04:12 -0500

    edit(build): set new config options
    I added three new config options in recent commits. This now sets them
    in the build script so I can take advantage of them.

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 expose="`pwd`/expose/expose.sh"
 export site_title="Photo Essays"
 export theme_dir="theme1-custom"
+#export resourcepath_prefix="https://s3.amazonaws.com/smallgren-stories/"
+#export asset_url_prefix="/stories/r/"
+#export nav_link_prefix="/stories/"
 
 cd src
 $expose -d
