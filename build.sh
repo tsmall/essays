a246bfdbbc2f6daf5b6b75950e21fb88ec377097
Tom Small <tsmall3@gmail.com>
2019-12-07 17:58:16 -0500

    Refactor build script to simplify switch b/w dev and prod

--- build.sh
+++ build.sh
@@ -1,11 +1,54 @@
 #!/usr/bin/env bash
 
+# ------------------------------------------------------------------------------
+# CONFIGURATION
+
+
+env=prod
+draft=on
+
+
+
+
+# ------------------------------------------------------------------------------
+# SCRIPT
+
+
 expose="`pwd`/expose/expose.sh"
 export site_title="Smallgren Stories"
 export theme_dir="theme1-custom"
-#export resourcepath_prefix="https://s3.amazonaws.com/smallgren-stories/"
-#export asset_url_prefix="/stories/r/"
-#export nav_link_prefix="/stories/"
+
+
+case "$env" in
+    "dev")
+        export resourcepath_prefix="/"
+        export asset_url_prefix="/"
+        export nav_link_prefix="/"
+        ;;
+
+    "prod")
+        export resourcepath_prefix="https://s3.amazonaws.com/smallgren-stories/"
+        export asset_url_prefix="/stories/r/"
+        export nav_link_prefix="/stories/"
+        ;;
+
+    *)
+        echo "Unknown environment: $env"
+        exit
+        ;;
+esac
+
+
+case "$draft" in
+    "on")
+        draft_flag="-d"
+        ;;
+
+    *)
+        draft_flag=""
+        ;;
+esac
+
 
 cd src
-$expose -d
+$expose $draft_flag
